98bfe7262ad4a6f734be93aa53c8dba2907a28a6
HAREESHRAVI <hareesh.unofficial@gmail.com>
2024-05-06 16:37:03 +0530

    added navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from '../../assets/hv.jpg'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="navbar">
+        <div className='navbar-left'> 
+            <img src={logo} alt="navbar-logo" className="navbar-logo" />
+            <h2>HoloVisison.io</h2>
+        </div>
+    
+    <div className='navbar-right'>
+        <ul className="nav-menu">
+            <li className="nav-menu-item">Home</li>
+            <li className="nav-menu-item">About Us</li>
+            <li className="nav-menu-item">Mission</li>
+            <li className="nav-menu-item">Contact</li>
+        </ul>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
